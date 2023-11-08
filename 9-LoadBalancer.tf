8ee331bfd63973af8c1dd5c13781aa7f424d154a
AMohamed0 <abubakrm99@gmail.com>
2023-11-08 17:54:41 -0600

    redirect any http traffic to https

--- 9-LoadBalancer.tf
+++ 9-LoadBalancer.tf
@@ -23,18 +23,18 @@ resource "aws_lb_listener" "http" {
   port              = 80
   protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app1_tg.arn
-  }
-
-  #   default_action {
-  #   type = "redirect"
-
-  #   redirect {
-  #     port        = "443"
-  #     protocol    = "HTTPS"
-  #     status_code = "HTTP_301"
-  #   }
+  # default_action {
+  #   type             = "forward"
+  #   target_group_arn = aws_lb_target_group.app1_tg.arn
   # }
+
+    default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
 }
