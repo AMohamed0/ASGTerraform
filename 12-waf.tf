68eeca7bb0d4ef0ab57254ed1880e790cb5aa75f
AMohamed0 <abubakrm99@gmail.com>
2023-11-08 23:55:26 -0600

    updated waf file

--- 12-waf.tf
+++ 12-waf.tf
@@ -1,42 +1,119 @@
-resource "aws_wafregional_ipset" "ipset" {
-  name = "tfIPSet"
-
-  ip_set_descriptor {
-    type  = "IPV4"
-    value = "192.0.7.0/24"
-  }
-}
-
-resource "aws_wafregional_rule" "foo" {
-  name        = "tfWAFRule"
-  metric_name = "tfWAFRule"
-
-  predicate {
-    data_id = aws_wafregional_ipset.ipset.id
-    negated = false
-    type    = "IPMatch"
-  }
-}
-
-resource "aws_wafregional_web_acl" "foo" {
-  name        = "foo"
-  metric_name = "foo"
+resource "aws_wafv2_web_acl" "WafWebAcl" {
+  name  = "wafv2-web-acl"
+  scope = "REGIONAL"
 
   default_action {
-    type = "ALLOW"
+    allow {}
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "WAF_Common_Protections"
+    sampled_requests_enabled   = true
   }
 
   rule {
-    action {
-      type = "BLOCK"
+    name     = "AWS-AWSManagedRulesCommonRuleSet"
+    priority = 0
+    override_action {
+      none {}
     }
-
-    priority = 1
-    rule_id  = aws_wafregional_rule.foo.id
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+        # Excluded rules can be specified here if needed
+        # excluded_rules {
+        #   name = "SizeRestrictions_BODY"
+        # }
+        # excluded_rules {
+        #   name = "NoUserAgent_HEADER"
+        # }
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled   = true
+    }
   }
+
+  # ... other rules here ...
+
+  # Example for additional rules (make sure to increment the priority for each rule)
+  rule {
+    name     = "AWS-AWSManagedRulesLinuxRuleSet"
+    priority = 1
+    override_action {
+      none {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesLinuxRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesLinuxRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesWindowsRuleSet"
+    priority = 1
+    override_action {
+      none {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWS-AWSManagedRulesWindowsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesWindowsRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 1
+    override_action {
+      none {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }  
+
+  # Continue to define other rules as needed, incrementing the priority each time
+
+  # Don't forget to close the resource block with a closing brace.
 }
 
-resource "aws_wafregional_web_acl_association" "foo" {
-  resource_arn = aws_lb.app1_alb.arn
-  web_acl_id   = aws_wafregional_web_acl.foo.id
+resource "aws_cloudwatch_log_group" "WafWebAclLoggroup" {
+  name              = "aws-waf-logs-wafv2-web-acl"
+  retention_in_days = 30
+}
+
+resource "aws_wafv2_web_acl_logging_configuration" "WafWebAclLogging" {
+  log_destination_configs = [aws_cloudwatch_log_group.WafWebAclLoggroup.arn]
+  resource_arn            = aws_wafv2_web_acl.WafWebAcl.arn
+}
+
+resource "aws_wafv2_web_acl_association" "WafWebAclAssociation" {
+  resource_arn = aws_lb.app1_alb.arn  # Ensure that this resource ARN is defined elsewhere in your Terraform
+  web_acl_arn  = aws_wafv2_web_acl.WafWebAcl.arn
 }
