28b6581cdba5c71895e20a76c7b36a82a686bd48
AMohamed0 <abubakrm99@gmail.com>
2023-11-08 22:27:38 -0600

    added waf to alb

---
+++ 12-waf.tf
@@ -0,0 +1,42 @@
+resource "aws_wafregional_ipset" "ipset" {
+  name = "tfIPSet"
+
+  ip_set_descriptor {
+    type  = "IPV4"
+    value = "192.0.7.0/24"
+  }
+}
+
+resource "aws_wafregional_rule" "foo" {
+  name        = "tfWAFRule"
+  metric_name = "tfWAFRule"
+
+  predicate {
+    data_id = aws_wafregional_ipset.ipset.id
+    negated = false
+    type    = "IPMatch"
+  }
+}
+
+resource "aws_wafregional_web_acl" "foo" {
+  name        = "foo"
+  metric_name = "foo"
+
+  default_action {
+    type = "ALLOW"
+  }
+
+  rule {
+    action {
+      type = "BLOCK"
+    }
+
+    priority = 1
+    rule_id  = aws_wafregional_rule.foo.id
+  }
+}
+
+resource "aws_wafregional_web_acl_association" "foo" {
+  resource_arn = aws_lb.app1_alb.arn
+  web_acl_id   = aws_wafregional_web_acl.foo.id
+}
